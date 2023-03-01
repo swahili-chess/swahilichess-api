9bb30b046ea5c63834ac90ead9523e8d25a31fee
Hopertz <lugano.ngulwa@gmail.com>
2023-03-01 22:19:33 +0300

    Add comments to custom validator

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -4,34 +4,47 @@ import (
 	"regexp"
 )
 
+// email regex
 var (
 	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
+
+
 
 type Validator struct {
 	Errors map[string]string
 }
 
+
+// Creates a new validator
 func New() *Validator {
 	return &Validator{Errors: make(map[string]string)}
 }
 
+
+// Checks if there is no errors  Validator Errors field
 func (v *Validator) Valid() bool {
 	return len(v.Errors) == 0
 }
 
+
+// Adds error if it doesn't current exists in error field in Validator struct
 func (v *Validator) AddError(key, message string) {
 	if _, exists := v.Errors[key]; !exists {
 		v.Errors[key] = message
 	}
 }
 
+
+// Uses AddError to add error when the condition is false.
 func (v *Validator) Check(ok bool, key, message string) {
 	if !ok {
 		v.AddError(key, message)
 	}
 }
 
+
+// Check the value if its in the List.
 func In(value string, list ...string) bool {
 	for i := range list {
 		if value == list[i] {
@@ -42,6 +55,8 @@ func In(value string, list ...string) bool {
 
 }
 
+
+// Matches the value with regex
 func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
 }
