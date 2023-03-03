51c9e0c48dcbef5b332608fcf3f9d62be97df1b4
Hopertz <lugano.ngulwa@gmail.com>
2023-03-03 16:12:44 +0300

    Create tokens  handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.sitecheckHandler)
 
 
 
