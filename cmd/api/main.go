c92cceedc31174ed3ba7d0e7a488e9c11cef091e
Hopertz <lugano.ngulwa@gmail.com>
2023-03-03 17:03:10 +0300

    Use new database dsn

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -54,7 +54,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", os.Getenv("ENV_STAGE"), "Environment (development|Staging|production")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("LEBENA_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("SW_DB_DSN"), "PostgreSQL DSN")
 
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max ilde connections")
