9cba245ec74daa33a552371e7dc7660fd0a8950a
Hopertz <lugano.ngulwa@gmail.com>
2023-03-05 10:46:44 +0300

    Update user table migrations

--- migrations/000002_create_user_table.up.sql
+++ migrations/000002_create_user_table.up.sql
@@ -5,8 +5,6 @@ CREATE TABLE IF NOT EXISTS users (
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     username text UNIQUE NOT NULL,
     email citext UNIQUE NOT NULL,
-    firstname text,
-    lastname text,
     password_hash bytea NOT NULL,
     activated bool NOT NULL,
     version integer NOT NULL DEFAULT 1
