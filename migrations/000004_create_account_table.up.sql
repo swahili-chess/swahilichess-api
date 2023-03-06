aac6b9c56bfd0d3636b8eff5412be996f09dcbf1
Hopertz <lugano.ngulwa@gmail.com>
2023-03-06 10:41:54 +0300

    Create account migrations

---
+++ migrations/000004_create_account_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    account_id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    firstname text UNIQUE NOT NULL,
+    lastname text UNIQUE NOT NULL,
+    lichess_username citext UNIQUE NOT NULL,
+    chesscom_username  citext UNIQUE NOT NULL,
+    phone_number text UNIQUE NOT NULL,
+);
+
