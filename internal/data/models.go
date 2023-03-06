589ccd9fd0b69eb2dec44da140679880cd92846c
Hopertz <lugano.ngulwa@gmail.com>
2023-03-06 13:29:51 +0300

    Add account model

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,11 +7,13 @@ import (
 type Models struct {
 	Users  UserModel
 	Tokens TokenModel
+	Accounts AccountModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Tokens: TokenModel{DB: db},
 		Users:  UserModel{DB: db},
+		Accounts: AccountModel{DB: db},
 	}
 }
