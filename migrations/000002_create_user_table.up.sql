93d0c63410c35815168a3d1a51036e83c99974be
Hopertz <lugano.ngulwa@gmail.com>
2023-03-06 10:43:37 +0300

    Update users table migrations

--- migrations/000002_create_user_table.up.sql
+++ migrations/000002_create_user_table.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION citext;
 CREATE TABLE IF NOT EXISTS users (
     uuid UUID PRIMARY KEY NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    username text UNIQUE NOT NULL,
+    username citext UNIQUE NOT NULL,
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
     activated bool NOT NULL,
