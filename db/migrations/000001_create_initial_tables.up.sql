45ef054e743d34a6e3b478e29904982612b68ebf
Hopertz <lugano.ngulwa@gmail.com>
2024-08-28 21:15:37 +0300

    update migrations  i.e add table of chessbot etc

--- db/migrations/000001_create_initial_tables.up.sql
+++ db/migrations/000001_create_initial_tables.up.sql
@@ -3,23 +3,29 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION citext;
 
 CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY NOT NULL,
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     username citext UNIQUE NOT NULL,
-    email citext UNIQUE NOT NULL,
     full_name text NOT NULL,
-    lichess_username citext UNIQUE NOT NULL,
+    lichess_username citext UNIQUE,
     chesscom_username  citext UNIQUE,
     phone_number text UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
     activated bool NOT NULL,
-    photos text NOT NULL,
+    photo text NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS tokens (
     hash bytea PRIMARY KEY,
-    uuid UUID NOT NULL REFERENCES users ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
     expiry timestamp(0) with time zone NOT NULL,
     scope text NOT NULL
 );
 
+-- Below  table used by the chessbot on nyumbani mates team, We only read here
+CREATE TABLE IF NOT EXISTS lichess (
+    id SERIAL PRIMARY KEY,
+    lichess_id TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP(0) NOT NULL DEFAULT NOW()
+);
