d4f2c2fbc23e6f489ea01c87167f7bf99e754ec1
Hopertz <lugano.ngulwa@gmail.com>
2024-08-30 14:22:17 +0300

    update routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,46 +1,36 @@
 package main
 
 import (
-	"expvar"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func (app *application) routes() http.Handler {
-	router := httprouter.New()
+func (app *application) routes() *echo.Echo {
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.sitecheckHandler)
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
+	DefaultCORSConfig := middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}
 
+	e.Use(middleware.CORSWithConfig(DefaultCORSConfig))
 
-	// User Routes
+	//router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
 
-	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	e.GET("/ping", app.pingHandler)
+	e.POST("/login", app.createAuthTokenHandler)
+	g := e.Group("/auth")
+	g.Use(app.authenticate)
+	g.PUT("/users/:id", app.updateUserHandler)
+	g.POST("/users/:id", app.registerUserHandler)
+	g.POST("/users/activate", app.activateUserHandler)
 
-    // User profile
-	router.HandlerFunc(http.MethodGet, "/v1/user/profile", app.requireActivatedUser(app.showProfile))
-	router.HandlerFunc(http.MethodPost, "/v1/user/create_profile", app.requireActivatedUser(app.createProfile))
-	router.HandlerFunc(http.MethodPut, "/v1/user/update_profile", app.requireActivatedUser(app.updateProfile))
-
-	// Resend user token route
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
-
-
-	// Metrics Routes
-	router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
-
-
-	//password management
-
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/passwordreset",app.createPasswordResetTokenHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
-
-	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
+	return e
 
 }
