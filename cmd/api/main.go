2d265d500d7b81f72c951f07177aa81430fdfc1a
Hopertz <lugano.ngulwa@gmail.com>
2024-08-31 08:59:32 +0300

    update main,go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"expvar"
 	"flag"
 	"log/slog"
@@ -11,41 +9,21 @@ import (
 	"sync"
 	"time"
 
-	"backend.chesswahili.com/internal/data"
-	"backend.chesswahili.com/internal/mailer"
+	"backend.chesswahili.com/config"
+	db "backend.chesswahili.com/internal/db/sqlc"
+	"backend.chesswahili.com/internal/nextsms"
+	"github.com/go-playground/validator/v10"
 	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
-type config struct {
-	port int
-	env  string
-	db   struct {
-		dsn          string
-		maxOpenConns int
-		maxIdleConns int
-		maxIdleTime  string
-	}
-
-	limiter struct {
-		rps     float64
-		burst   int
-		enabled bool
-	}
-
-	mailergun struct {
-		domain     string
-		privateKey string
-		sender     string
-	}
-}
-
 type application struct {
-	config config
-	models data.Models
-	mailer mailer.Mailer
-	wg     sync.WaitGroup
+	config    config.Config
+	store     db.Store
+	wg        sync.WaitGroup
+	validator *validator.Validate
+	nextsms   nextsms.NextSmS
 }
 
 func init() {
@@ -58,53 +36,45 @@ func init() {
 }
 
 func main() {
-	var cfg config
+	var cfg config.Config
 
-	flag.IntVar(&cfg.port, "port", 4040, "API server port")
-	flag.StringVar(&cfg.env, "env", os.Getenv("ENV_STAGE"), "Environment (development|Staging|production")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("SW_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.PORT, "port", 4040, "API server port")
+	flag.StringVar(&cfg.ENV, "env", os.Getenv("ENV_STAGE"), "Environment (development|Staging|production")
+	flag.StringVar(&cfg.DB.DSN, "db-dsn", os.Getenv("SW_DB_DSN"), "PostgreSQL DSN")
 
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max ilde connections")
-	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection  connections")
+	flag.StringVar(&cfg.NextSmS.Username, "nextsms-username", os.Getenv("NEXTSMS_USERNAME"), "nextsms-username")
+	flag.StringVar(&cfg.NextSmS.Password, "nextsms-password", os.Getenv("NEXTSMS_PASSWORD"), "nextsms-password")
 
-	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
-	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
-	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
-
-	flag.StringVar(&cfg.mailergun.domain, "mg-domain", os.Getenv("MAILGUN_DOMAIN"), "Mailgun-domain")
-	flag.StringVar(&cfg.mailergun.privateKey, "mg-privatekey", os.Getenv("MAILGUN_PRIVATEKEY"), "Mailgun-privatekey")
-	flag.StringVar(&cfg.mailergun.sender, "mg-sender", os.Getenv("MAILGUN_SENDER"), "Mailgun-sender")
+	flag.IntVar(&cfg.DB.MaxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.DB.MaxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max ilde connections")
+	flag.StringVar(&cfg.DB.MaxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection  connections")
 
 	flag.Parse()
 
-	db, err := openDB(cfg)
+	conn, err := config.OpenDB(cfg)
 	if err != nil {
 		slog.Error("failed to establish connection to db", "error", err)
 		return
 	}
-
-	defer db.Close()
+	defer conn.Close()
 	slog.Info("database connection pool established")
 
 	expvar.NewString("version").Set(version)
-
 	expvar.Publish("goroutines", expvar.Func(func() interface{} {
 		return runtime.NumGoroutine()
 	}))
-
 	expvar.Publish("database", expvar.Func(func() interface{} {
-		return db.Stats()
+		return conn.Stats()
 	}))
-
 	expvar.Publish("timestamp", expvar.Func(func() interface{} {
 		return time.Now().Unix()
 	}))
 
 	app := &application{
-		config: cfg,
-		models: data.NewModels(db),
-		mailer: mailer.New(cfg.mailergun.domain, cfg.mailergun.privateKey, cfg.mailergun.sender),
+		config:    cfg,
+		store:     db.NewStore(conn),
+		validator: validator.New(),
+		nextsms:   nextsms.New(cfg.NextSmS.Username, cfg.NextSmS.Password),
 	}
 
 	err = app.serve()
@@ -113,33 +83,4 @@ func main() {
 		return
 	}
 
-}
-
-func openDB(cfg config) (*sql.DB, error) {
-	db, err := sql.Open("postgres", cfg.db.dsn)
-
-	if err != nil {
-		return nil, err
-	}
-
-	db.SetMaxOpenConns(cfg.db.maxOpenConns)
-	db.SetMaxIdleConns(cfg.db.maxIdleConns)
-
-	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
-	if err != nil {
-		return nil, err
-	}
-
-	db.SetConnMaxIdleTime(duration)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	err = db.PingContext(ctx)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
