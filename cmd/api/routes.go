212e291db8fdee1bf71a68c4b291bc66d8c9388f
Hopertz <lugano.ngulwa@gmail.com>
2024-09-13 10:55:52 +0300

    format routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,12 +23,16 @@ func (app *application) routes() *echo.Echo {
 
 	e.GET("/ping", app.pingHandler)
 	e.POST("/login", app.createAuthTokenHandler)
-	e.POST("/users", app.registerUserHandler)
 	e.GET("/lichess/leaderboard", app.leaderboardHandler)
+
+	// user management
+	e.POST("/users", app.registerUserHandler)
 	e.POST("/users/activate", app.activateUserHandler)
 	e.POST("/users/resend/activation", app.resendactivationHandler)
 	e.POST("/users/forgot-password", app.forgotPasswordUserHandler)
 	e.POST("/users/change-password", app.changePasswordUserHandler)
+
+	//TODO add ability to change phone number
 
 	g := e.Group("/auth")
 	g.Use(app.authenticate)
